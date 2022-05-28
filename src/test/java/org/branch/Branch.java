9b892ec7d02a5876db199237ef0dbf7bec85abe6
sasikiran0105 <sasikiransusanth@gmail.com>
2022-05-28 15:18:44 +0530

    methodsadded

---
+++ src/test/java/org/branch/Branch.java
@@ -0,0 +1,24 @@
+package org.branch;
+
+public class Branch {
+
+	public void Method1() {
+		System.out.println("1");
+	}
+public void Method2() {
+	System.out.println("2");
+	}
+public void Method3() {
+	System.out.println("3");
+}
+public void Method4() {
+	System.out.println("4");
+}
+public void Method5() {
+	System.out.println("5");
+}
+public void Method6() {
+	System.out.println("6");
+}
+	
+}
